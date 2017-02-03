5803e58a2d75d3b87eaf7355712968b04c161cdc
Richard-ML <Richard.ML@ubernetic.ca>
2017-02-03 03:51:48 -0500

    Started setup of gTestEngine.

---
+++ gTestEngine/externals.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "gtest\gtest.h"
+// TODO: OTHERS... Do the same singleton implementation as the Physics Engine? :P 
---
+++ gTestEngine/gTestEngine.cpp
@@ -0,0 +1,6 @@
+// gTestEngine.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
